6e367a2daf0d83f5b335f6dee6cf09d8df66a9db
Baker Jackson <bakerwjackson@gmail.com>
2021-05-07 17:16:01 -0400

    multiple server support

--- main.py
+++ main.py
@@ -1,81 +1,69 @@
+import os
+import random
+from itertools import combinations
+
 import discord
 from discord.ext import commands
-from dataclasses import dataclass
-from itertools import combinations
-import random
-import os
+
+from server import Player, TeamCombination, Server
 
 bot = commands.Bot(command_prefix='!', help_command=None)
 
+servers = {}
 
-@dataclass
-class Player:
-    """
-    The Player class represents a discord user. The rank value
-    is determined based off of their Valorant Competitive rank.
-    """
-    user: str
-    rank: int
-
-    def __str__(self):
-        return self.user
-
-
-@dataclass
-class TeamCombination:
-    """
-    The TeamCombination class represents a possible pairing
-    of 5 players vs 5 players. The evaluation is the difference
-    between these teams and perfectly fair teams
-    """
-    team1: list[Player]
-    team2: list[Player]
-    evaluation: float
-
-# RANKS is a dictionairy that maps a Valorant rank to elo
+# RANKS is a dictionary that maps a Valorant rank to elo
 RANKS = {
-    'bronze1': 640,
-    'bronze2': 670,
-    'bronze3': 710,
-    'silver1': 800,
-    'silver2': 830,
-    'silver3': 860,
-    'gold1': 960,
-    'gold2': 990,
-    'gold3': 1020,
-    'plat1': 1050,
-    'plat2': 1080,
-    'plat3': 1110
+    'iron1': 575,
+    'iron2': 590,
+    'iron3': 600,
+    'bronze1': 620,
+    'bronze2': 640,
+    'bronze3': 660,
+    'silver1': 680,
+    'silver2': 700,
+    'silver3': 720,
+    'gold1': 740,
+    'gold2': 760,
+    'gold3': 780,
+    'plat1': 800,
+    'plat2': 820,
+    'plat3': 840,
+    'diamond1': 860,
+    'diamond2': 880,
+    'diamond3': 900
 }
 
-CURRENT_PLAYERS = []
 
-TEAMS = []
+def get_server(guild) -> Server:
+    return servers[guild]
 
-CURRENT_TEAM: int = 0
 
-TEAM1 = []
-TEAM2 = []
+@bot.before_invoke
+async def create_guild(ctx):
+    print(ctx.guild.id)
+    if ctx.guild not in servers:
+        servers[ctx.guild] = Server()
 
-GAME_OVER = True
 
 @bot.command()
 async def help(ctx):
-    embed=discord.Embed(title="5v5 Bot Help")
-    embed.set_thumbnail(url="https://yt3.ggpht.com/ytc/AAUvwnh5cX3Hpigfm2Y3X1VAd1QrVBWgzFeaIM8RAuTu=s900-c-k-c0x00ffffff-no-rj")
+    embed = discord.Embed(title="5v5 Bot Help")
+    embed.set_thumbnail(
+        url="https://yt3.ggpht.com/ytc/AAUvwnh5cX3Hpigfm2Y3X1VAd1QrVBWgzFeaIM8RAuTu=s900-c-k-c0x00ffffff-no-rj")
     embed.add_field(name="!join (rank)", value="Adds you to the 5v5 (!join gold2)", inline=False)
     embed.add_field(name="!players", value="Displays all players in the 5v5", inline=False)
     embed.add_field(name="!maketeams", value="Makes teams according to rankings", inline=False)
     embed.add_field(name="!next", value="Shows a new variation of teams", inline=False)
     embed.add_field(name="!choose", value="Locks in the teams", inline=False)
-    embed.add_field(name="!winner (0,1,2)", value="Input the winner of the 5v5 (!winner 1 for Team 1, 0 for a tie)", inline=False)
+    embed.add_field(name="!winner (0,1,2)", value="Input the winner of the 5v5 (!winner 1 for Team 1, 0 for a tie)",
+                    inline=False)
     embed.add_field(name="!boost", value="Slightly increases the ranking of a player", inline=False)
     embed.add_field(name="!kick (player-name)", value="Kicks a player from the 5v5", inline=False)
     embed.add_field(name="!leave", value="Removes you from the 5v5", inline=False)
     embed.add_field(name="!reset", value="Removes all players", inline=False)
     embed.add_field(name="!randommap", value="Gives a random map", inline=False)
     await ctx.send(embed=embed)
-    
+
 
 def win_probability(team1: list[Player], team2: list[Player]) -> float:
     """
@@ -94,8 +82,10 @@ async def join(ctx, rank):
     The join function adds a user to the current party if they send the message
     !join <current rank>
     """
+    server = get_server(ctx.guild)
+    current_players = server.current_players
     player = ctx.author
-    for p in CURRENT_PLAYERS:
+    for p in current_players:
         if p.user == str(player):
             await ctx.send(str(player) + ' has already been added to the game!')
             return
@@ -103,11 +93,11 @@ async def join(ctx, rank):
         await ctx.send("Please give a valid rank!")
         return
     elo = RANKS[rank]
-    if len(CURRENT_PLAYERS) >= 10:
+    if len(current_players) >= 10:
         await ctx.send('There are already 10 people in the game')
         return
-    CURRENT_PLAYERS.append(Player(str(player), elo))
-    embed=discord.Embed()
+    current_players.append(Player(str(player), elo))
+    embed = discord.Embed()
     embed.add_field(name="Join success", value=str(player) + " has successfully joined!", inline=False)
     await ctx.send(embed=embed)
 
@@ -118,12 +108,12 @@ async def reset(ctx):
     The reset function removes all players from the current party and resets
     all of the global variables
     """
-    CURRENT_PLAYERS.clear()
-    TEAM1.clear()
-    TEAM2.clear()
-    TEAMS.clear()
-    global CURRENT_TEAM
-    CURRENT_TEAM = 0
+    server = get_server(ctx.guild)
+    server.current_players.clear()
+    server.team1.clear()
+    server.team2.clear()
+    server.teams.clear()
+    server.current_team = 0
     await ctx.send("Current players has been reset")
 
 
@@ -147,8 +137,9 @@ async def players(ctx):
     """
     players returns a list of all the players currently in the party
     """
-    embed=discord.Embed()
-    embed.add_field(name="Players", value=format_players(CURRENT_PLAYERS), inline=False)
+    server = get_server(ctx.guild)
+    embed = discord.Embed()
+    embed.add_field(name="Players", value=format_players(server.current_players), inline=False)
     await ctx.send(embed=embed)
 
 
@@ -158,36 +149,39 @@ async def maketeams(ctx):
     maketeams checks every combination of the party and returns a team
     that is relatively equal in strength
     """
-    global CURRENT_TEAM, TEAMS
-    CURRENT_TEAM = 0
-    TEAMS = []
-    if len(CURRENT_PLAYERS) != 10:
+    server = get_server(ctx.guild)
+    server.current_team = 0
+    server.teams = []
+    if len(server.current_players) != 10:
         await ctx.send('Please get 10 players!')
         return
-    for combo in combinations(CURRENT_PLAYERS, 5):
-        team2 = [x for x in CURRENT_PLAYERS if x not in combo]
+    for combo in combinations(server.current_players, 5):
+        team2 = [x for x in server.current_players if x not in combo]
         evaluation = abs(0.5 - win_probability(combo, team2))
-        TEAMS.append(TeamCombination(combo, team2, evaluation))
-    TEAMS.sort(key=lambda x: x.evaluation)
-    TEAMS = TEAMS[:20]
-    random.shuffle(TEAMS)
+        server.teams.append(TeamCombination(combo, team2, evaluation))
+    server.teams.sort(key=lambda x: x.evaluation)
+    server.teams = server.teams[:20]
+    random.shuffle(server.teams)
 
-    embed=discord.Embed()
-    embed.add_field(name="5v5 Teams", value='Team 1:\n' + format_players(TEAMS[0].team1) + '\nTeam 2:\n' + format_players(TEAMS[0].team2), inline=False)
+    embed = discord.Embed()
+    embed.add_field(name="5v5 Teams",
+                    value='Team 1:\n' + format_players(server.teams[0].team1) + '\nTeam 2:\n' + format_players(
+                        server.teams[0].team2), inline=False)
     await ctx.send(embed=embed)
 
 
 @bot.command()
 async def next(ctx):
     """
-    newteams gives another team combination of similar fairness
+    next gives another team combination of similar fairness
     """
-    global CURRENT_TEAM
-    CURRENT_TEAM += 1
-    if CURRENT_TEAM < len(TEAMS):
-        embed=discord.Embed()
-        embed.add_field(name="5v5 Teams", value='Team 1:\n' + format_players(TEAMS[CURRENT_TEAM].team1) + '\nTeam 2:\n' + format_players(
-            TEAMS[CURRENT_TEAM].team2), inline=False)
+    server = get_server(ctx.guild)
+    server.current_team += 1
+    if server.current_team < len(server.teams):
+        embed = discord.Embed()
+        embed.add_field(name="5v5 Teams", value='Team 1:\n' + format_players(
+            server.teams[server.current_team].team1) + '\nTeam 2:\n' + format_players(
+            server.teams[server.current_team].team2), inline=False)
         await ctx.send(embed=embed)
     else:
         await ctx.send("Teams have not been made")
@@ -198,10 +192,10 @@ async def choose(ctx):
     """
     choose locks in the current team choices and the game begins!
     """
-    global TEAM1, TEAM2, GAME_OVER
-    TEAM1 = TEAMS[CURRENT_TEAM].team1
-    TEAM2 = TEAMS[CURRENT_TEAM].team2
-    GAME_OVER = False
+    server = get_server(ctx.guild)
+    server.team1 = server.teams[server.current_team].team1
+    server.team2 = server.teams[server.current_team].team2
+    server.game_over = False
     await ctx.send('glhf!')
 
 
@@ -211,23 +205,23 @@ async def winner(ctx, val):
     winner tells the bot who won the game - Team 1 or Team 2.
     The team ranks are adjusted accordingly
     """
-    global GAME_OVER, TEAM1, TEAM2
-    if GAME_OVER:
+    server = get_server(ctx.guild)
+    if server.game_over:
         await ctx.send('There is no game right now - get some friends and make teams')
         return
     if val not in {'0', '1', '2'}:
         await ctx.send('Not a valid team!')
     if val == '1':
-        for player in TEAM1:
+        for player in server.team1:
             player.rank += 8
-        for player in TEAM2:
+        for player in server.team2:
             player.rank -= 8
     elif val == '2':
-        for player in TEAM2:
+        for player in server.team2:
             player.rank += 8
-        for player in TEAM1:
+        for player in server.team1:
             player.rank -= 8
-    GAME_OVER = True
+    server.game_over = True
     await ctx.send('GGWP! Want to play another?')
 
 
@@ -237,13 +231,13 @@ async def boost(ctx):
     boost slightly increases a player's rank to deal with the possibility
     of their abilities being higher than their rank
     """
+    server = get_server(ctx.guild)
     player = str(ctx.author)
-    for p in CURRENT_PLAYERS:
+    for p in server.current_players:
         if p.user == player:
             p.rank += 8
-            cur = p
             break
-    await ctx.send(cur.user + "'s elo has been increased")
+    await ctx.send(player + "'s elo has been increased")
 
 
 @bot.command()
@@ -251,10 +245,11 @@ async def leave(ctx):
     """
     leave removes a player from the current party
     """
+    server = get_server(ctx.guild)
     player = str(ctx.author)
-    for p in CURRENT_PLAYERS:
+    for p in server.current_players:
         if p.user == player:
-            CURRENT_PLAYERS.remove(p)
+            server.current_players.remove(p)
             break
     await ctx.send('You have left the party. We hope you had fun!')
 
@@ -264,10 +259,11 @@ async def kick(ctx, player):
     """
     kick forces a player out of the current lobby
     """
+    server = get_server(ctx.guild)
     kicked = False
-    for p in CURRENT_PLAYERS:
+    for p in server.current_players:
         if p.user == player:
-            CURRENT_PLAYERS.remove(p)
+            server.current_players.remove(p)
             kicked = True
             break
     if kicked:
@@ -278,6 +274,7 @@ async def kick(ctx, player):
 
 @bot.command()
 async def randommap(ctx):
-    await ctx.send(random.choice(['Bind', 'Split', 'Haven', 'Icebox']))
+    await ctx.send(random.choice(['Bind', 'Split', 'Haven', 'Icebox', 'Breeze']))
+
 
 bot.run(os.getenv('BOT-KEY'))
---
+++ server.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Player:
+    """
+    The Player class represents a discord user. The rank value
+    is determined based off of their Valorant Competitive rank.
+    """
+    user: str
+    rank: int
+
+    def __str__(self):
+        return self.user
+
+
+@dataclass
+class TeamCombination:
+    """
+    The TeamCombination class represents a possible pairing
+    of 5 players vs 5 players. The evaluation is the difference
+    between these teams and perfectly fair teams
+    """
+    team1: list[Player]
+    team2: list[Player]
+    evaluation: float
+
+
+class Server:
+    """
+    Represents one discord servers - stores all info related to said server
+    """
+    current_players: list[Player]
+    teams: list[TeamCombination]
+    current_team: int
+    team1: TeamCombination
+    team2: TeamCombination
+    game_over: bool
+
+    def __init__(self):
+        self.current_team = 0
+        self.current_players = []
+        self.teams = []
+        self.game_over = True
